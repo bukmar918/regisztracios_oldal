8287181cc4d07919b7793c5b4b0322ac63bb9dad
Mucsi Márton Balázs 107 <mucmar107@logiker.hu>
2025-12-11 10:33:40 +0100

    megvan

--- regisztracios-oldal/app.py
+++ regisztracios-oldal/app.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import hashlib
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template(
+        "register.html"
+    )
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    form = {
+        
+        
+    }
